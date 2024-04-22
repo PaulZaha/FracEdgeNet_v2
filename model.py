69b25bf33df759b1ee4e13e8749a957b69b9295f
Paul Zaha <paul.zaha@uni-bayreuth.de>
2024-04-22 07:59:39 +0200

    classification model

---
+++ model.py
@@ -0,0 +1,122 @@
+import tensorflow as tf
+from keras import layers
+import matplotlib.pyplot as plt
+import keras
+
+def EfficientNetB4(train_generator,validation_generator,test_generator):
+    
+    input_layer = layers.Input(shape=(380,380,3))
+
+    model_EfficientNetB4 = tf.keras.applications.EfficientNetB4(weights='imagenet',input_tensor = input_layer,include_top = False)
+
+    flatten = tf.keras.layers.Flatten()
+    classifier = tf.keras.layers.Dense(1,activation='sigmoid')
+
+    model = tf.keras.models.Sequential([
+        model_EfficientNetB4,
+        flatten,
+        classifier
+    ])
+    #Layer untrainable machen
+    for layer in model.layers[:-1]: #auf -1 ändern, wenn nur der finale classifier und keine Dense schicht
+        layer.trainable=False
+        
+    model_compiler(model)
+
+    print("Ab hier: Classifier Fitting")
+    model_fitter(model,train_generator,validation_generator,2)
+
+    model_evaluater(test_generator)
+
+
+
+#optimized learning rate for hypertuning EfficientNetB4
+def model_compiler(model):
+    initial_lr = 1e-4
+    lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+        initial_lr,
+        decay_steps=130,
+        decay_rate=0.94,
+        staircase=True
+    )
+    print("scheduler jetzt da")
+    model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
+            loss=tf.keras.losses.BinaryCrossentropy(),
+            metrics=['accuracy','FalseNegatives','FalsePositives','Precision','Recall'])
+
+def model_fitter(model,train_generator,validation_generator,epochs):
+    history = model.fit(train_generator,validation_data=validation_generator,epochs=epochs
+                        ,class_weight = {0: 1, 1: 5}
+                        #,callbacks =[model_callback,stopper,tensorboard_callback,learning_rate_scheduler,custom_lr_update_callback]
+                        #,steps_per_epoch=500
+                        )
+
+    #Create plots
+    plt.figure(figsize=(12, 6))
+
+    #Plot training and validation accuracy
+    plt.subplot(1, 2, 1)
+    plt.plot(history.history['accuracy'], label='Training Accuracy')
+    plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    #Plot training and validation loss
+    plt.subplot(1, 2, 2)
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    # Adjust layout to prevent overlapping
+    plt.tight_layout()
+
+    # Show the plots
+    plt.show()
+
+def model_evaluater(test_generator):
+    #Load model from best epoch
+    model = tf.keras.models.load_model(os.path.join(os.getcwd(),"bestmodel.h5"))
+
+    #Evaluate the model and save to results
+    results = model.evaluate(test_generator)
+
+    #Create dict with evaluation metrics for testing dataset
+    eval_metrics = {"Accuracy: ": results[1]
+                    ,"Loss: ": results[0]
+                    ,"False_Negatives: ":int(results[2])
+                    ,"False_Positives: ":int(results[3])
+                    ,"Precision: ":results[4]
+                    ,"Recall: ":results[5]
+                    }
+    print(eval_metrics)
+
+
+    False_Negatives = int(eval_metrics['False_Negatives: '])
+    False_Positives = int(eval_metrics['False_Positives: '])
+
+    True_Positives = int((eval_metrics['Precision: ']*False_Positives)/(1-eval_metrics['Precision: ']))
+    True_Negatives = int((test_generator.n)-True_Positives-False_Negatives-False_Positives)
+
+    Precicion = eval_metrics['Precision: ']
+    Recall = eval_metrics['Recall: ']
+    Accuracy = eval_metrics['Accuracy: ']
+
+
+    confusion_matrix = np.array([[True_Positives,False_Positives],[False_Negatives,True_Negatives]])
+
+    print('Confusion Matrix:')
+    print(confusion_matrix)
+
+    print('Accuracy:')
+    print(Accuracy)
+
+    print('Precicion:')
+    print(Precicion)
+
+    print('Recall:')
+    print(Recall)
---
+++ pipeline.py
@@ -0,0 +1,103 @@
+#region imports and settings
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.patches as patches
+import tensorflow as tf
+import xml.etree.ElementTree as ET
+
+from keras.preprocessing.image import ImageDataGenerator
+
+import sklearn
+from sklearn.model_selection import KFold, train_test_split
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+from model import *
+
+
+def create_generators(train_df,test_df,targetsize):
+    """
+    Creates train_, validation_, and test_generator to feed Model with batches of data
+    """
+    #images pathing
+    path = os.path.join(os.getcwd(),'FracAtlas','images','all')
+    print(os.getcwd())
+    #Create DataGenerator for training and validation data with augmentation
+    #Note: For EfficientNet remove rescaling
+    datagen = ImageDataGenerator(#rescale = 1./255,
+                                                              rotation_range=10 
+                                                              ,width_shift_range=0.05
+                                                              ,height_shift_range=0.05
+                                                              ,validation_split=0.2)
+    
+    #Create DataGenerator for testing without augmentation
+    #Note: For EfficientNet remove rescaling
+    test_datagen = ImageDataGenerator(#rescale=1./255
+        )
+
+
+    train_generator = datagen.flow_from_dataframe(dataframe=train_df,
+                                                        directory=path,
+                                                        x_col='image_id',
+                                                        y_col='fractured',
+                                                        class_mode='binary',
+                                                        color_mode = 'rgb',
+                                                        shuffle=True,
+                                                        target_size=targetsize,
+                                                        subset='training'
+                                                        ,batch_size=16)
+    
+
+    validation_generator = datagen.flow_from_dataframe(dataframe=train_df,
+                                                        directory=path,
+                                                        x_col='image_id',
+                                                        y_col='fractured',
+                                                        class_mode='binary',
+                                                        color_mode = 'rgb',
+                                                        shuffle=True,
+                                                        target_size=targetsize,
+                                                        subset='validation'
+                                                        ,batch_size=16)
+
+    test_generator = test_datagen.flow_from_dataframe(dataframe=test_df,
+                                                      directory=path,
+                                                        x_col='image_id',
+                                                        y_col='fractured',
+                                                        class_mode='binary',
+                                                        color_mode = 'rgb',
+                                                        shuffle=True,
+                                                        target_size=targetsize,
+                                                        batch_size=1)
+
+    return train_generator, validation_generator, test_generator
+
+
+def main():
+    os.chdir(os.path.join(os.getcwd(),'Dataset','FracAtlas'))
+    dataframe = pd.read_csv('dataset.csv')
+    os.chdir(os.path.join(os.getcwd(),'..'))
+    
+    dataframe = dataframe[['image_id', 'fractured']].assign(fractured=dataframe['fractured'].astype(str))
+    print(dataframe)
+
+
+    train_dataset, test_dataset = train_test_split(dataframe, train_size = 0.9, shuffle = True)
+
+    #gewicht = round(((dataframe['fractured'].value_counts()).get(0,1))/(dataframe['fractured'].value_counts()).get(1,0),3)
+    
+    #Set targetsize
+    targetsize = (380,380)
+    
+    #Create generators from train/test-split with chosen targetsize
+    train_generator,validation_generator, test_generator = create_generators(train_dataset,test_dataset,targetsize)
+
+
+    EfficientNetB4(train_generator,validation_generator,test_generator)
+
+
+if __name__ == "__main__":
+    main()
